cef9d50b6cadefcf330ed253a738710eead28051
srivastavakshitijprofessional@gmail.com <srivastavakshitijprofessional@gmail.com>
2022-05-02 15:39:55 +0530

    initial

---
+++ contracts/Day4.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Day4 {
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function send(address payable[] memory to, uint256[] memory amount)
+        public
+        payable
+        ownerOnly
+    {
+        require(to.length == amount.length, "to must be same length as amount");
+        for (uint256 i = 0; i < to.length; i++) {
+            to[i].transfer(amount[i]); //to array  - 0x00 0x01 0x02
+            //amount array - 10 20 30
+        }
+    }
+
+    modifier ownerOnly() {
+        require(msg.sender == owner);
+        _;
+    }
+}
---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,5 @@
+const Day4 = artifacts.require("Day4");
+
+module.exports = function (deployer, accounts) {
+  deployer.deploy(Day4);
+};
